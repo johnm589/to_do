a88711f2a58620455c4760f9185124099c98edf1
John <johnm589@yahoo.com>
2015-11-17 11:22:22 -0800

    server

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ var express = require('express')
   , bodyParser = require('body-parser')
   , mongoose = require('mongoose')
   , app = express()
+  , ejs = require('ejs')
+
+
+
+
+  app.listen(3000),function(){
+    console.log("Server Running on port 3000")
+  }
